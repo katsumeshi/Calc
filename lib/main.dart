5ba83fb5f0f254bff2fe1608473ba8ee2b8b3ea0
Yuki Matsushita <ymatsushita@picniic.com>
2018-05-11 23:40:21 -0700

    add list view

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,6 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: new MyHomePage(title: 'Calc'),
@@ -26,15 +18,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -46,7 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   String _test = "";
 
-  // tricky to make 2d array in dart
+  // not sure to make 2d array in dart
   static List<String> keys1 = ['7', '8', '9', '/'];
   static List<String> keys2 = ['4', '5', '6', 'x'];
   static List<String> keys3 = ['1', '2', '3', '-'];
@@ -68,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 this._incrementCounter(number);
               },
               child: new Container(
-                margin: const EdgeInsets.symmetric(vertical: 30.0),
+                margin: const EdgeInsets.symmetric(vertical: 25.0),
                 child: new Text(number, style: new TextStyle(fontSize: 20.0)),
               )));
     }).toList());
@@ -83,7 +66,23 @@ class _MyHomePageState extends State<MyHomePage> {
           title: new Text(widget.title),
         ),
         body: new Column(children: <Widget>[
-          result,
+          new Expanded(
+              flex: 1,
+              child: new ListView(children: <Widget>[
+                new Container(
+                    // margin: const EdgeInsets.only(top: 20.0),
+                    alignment: Alignment.centerLeft,
+                    decoration: const BoxDecoration(
+                      border: const Border(
+                        bottom: const BorderSide(
+                            width: 2.0, color: const Color(0xFF33000000)),
+                      ),
+                    ),
+                    child: new Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: new Text('$_test',
+                            style: new TextStyle(fontSize: 15.0))))
+              ])),
           this.getRow(keys1),
           this.getRow(keys2),
           this.getRow(keys3),
